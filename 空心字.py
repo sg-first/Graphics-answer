c4722b0f383c0fd2ae19d7684c3e9313839ac65b
3A <524019351@qq.com>
2021-03-29 15:22:41 +0800

    简单的优化一波

--- 空心字.py
+++ 空心字.py
@@ -23,35 +23,39 @@ allLine=np.array(allLine)
 startPos=np.array(startPos)
 allLine=allLine-startPos
 
-'''
-num=len(allLine) # 一共多少根线，就是矩阵有多少行
-line1=ti.Matrix(allLine[:,0,:])
-line2=ti.Matrix(allLine[:,1,:])
-'''
-
 img = np.zeros((size, size))
 boundary_color=0.9
 
-def DDA(x0,y0,x1,y1):
+def bresenham(x0, y0, x1, y1):
     if x0>x1:
         x0,y0,x1,y1=x1,y1,x0,y0
-
-    step=0.04
 
     dx=x1-x0
     dy=y1-y0
     k=dy/dx
-    y=y0
+    step=0.7
 
+    y=y0
     x=x0
-    allPos=[]
-    while x<=x1:
-        rx=int(x)
-        ry=size-int(y+0.5)
-        img[rx,ry]=boundary_color # y+0.5相当于四舍五入，size-规范化坐标
-        allPos.append((rx,ry))
-        y+=k*step
-        x+=step
+    allPos = []
+
+    if dx>=dy:
+        while x<=x1:
+            rx=int(x)
+            ry=size-int(y+0.5)
+            img[rx,ry]=boundary_color # y+0.5相当于四舍五入，size-规范化坐标
+            allPos.append((rx,ry))
+            y += k * step
+            x += step
+    else:
+        k=1/k
+        while y <= y1:
+            rx = int(x + 0.5)
+            ry = size - int(y)
+            img[rx, ry] = boundary_color  # y+0.5相当于四舍五入，size-规范化坐标
+            allPos.append((rx, ry))
+            y += step
+            x += k*step
     return allPos
 
 
@@ -104,20 +108,17 @@ def drawName(x,y,x1,y1):
     for l1,l2 in allLine:
         l11=l1+d
         l22=l2+d
-        allPos=DDA(l11[0],l11[1],l22[0],l22[1])
+        allPos=bresenham(l11[0], l11[1], l22[0], l22[1])
         l11d=l1+d1
         l22d=l2+d1
-        allPos+=DDA(l11d[0],l11d[1],l22d[0],l22d[1])
-        allPos+=DDA(l11[0],l11[1],l11d[0],l11d[1])
-        allPos+=DDA(l22[0],l22[1],l22d[0],l22d[1])
+        allPos+=bresenham(l11d[0], l11d[1], l22d[0], l22d[1])
+        allPos+=bresenham(l11[0], l11[1], l11d[0], l11d[1])
+        allPos+=bresenham(l22[0], l22[1], l22d[0], l22d[1])
         fill(l11,l11d,l22,l22d)
         setColor(allPos)
 
-def draw():
-    drawName(20, 70, 50, 100)
-
 gui = ti.GUI("Taichi", res=size)
 for frame in range(20000):
-    draw()
+    drawName(20, 70, 50, 100)
     gui.set_image(img)
     gui.show() # Change to gui.show(f'{frame:06d}.png') to write images to disk
