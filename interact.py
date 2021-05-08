083e66b78807c9ac85951cd37e7baa0252247754
3A <524019351@qq.com>
2021-05-08 15:38:54 +0800

    封装一下3d demo

--- interact.py
+++ interact.py
@@ -107,3 +107,63 @@ def keydown(key, x, y):
     elif key == b' ':  # 空格键，切换投影模式
         IS_PERSPECTIVE = not IS_PERSPECTIVE
         glutPostRedisplay()
+
+def reshape(width, height):
+    global WIN_W, WIN_H
+
+    WIN_W, WIN_H = width, height
+    glutPostRedisplay()
+
+def init():
+    glClearColor(0.0, 0.0, 0.0, 1.0)  # 设置画布背景色。注意：这里必须是4个参数
+    glEnable(GL_DEPTH_TEST)  # 开启深度测试，实现遮挡关系
+    glDepthFunc(GL_LEQUAL)  # 设置深度测试函数（GL_LEQUAL只是选项之一）
+
+def drawInit():
+    global IS_PERSPECTIVE, VIEW
+    global EYE, LOOK_AT, EYE_UP
+    global SCALE_K
+    global WIN_W, WIN_H
+
+    # 清除屏幕及深度缓存
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+    # 设置投影（透视投影）
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+
+    if WIN_W > WIN_H:
+        if IS_PERSPECTIVE:
+            glFrustum(VIEW[0] * WIN_W / WIN_H, VIEW[1] * WIN_W / WIN_H, VIEW[2],
+                      VIEW[3],
+                      VIEW[4], VIEW[5])
+        else:
+            glOrtho(VIEW[0] * WIN_W / WIN_H, VIEW[1] * WIN_W / WIN_H, VIEW[2],
+                    VIEW[3],
+                    VIEW[4], VIEW[5])
+    else:
+        if IS_PERSPECTIVE:
+            glFrustum(VIEW[0], VIEW[1], VIEW[2] * WIN_H / WIN_W,
+                      VIEW[3] * WIN_H / WIN_W,
+                      VIEW[4], VIEW[5])
+        else:
+            glOrtho(VIEW[0], VIEW[1], VIEW[2] * WIN_H / WIN_W,
+                    VIEW[3] * WIN_H / WIN_W,
+                    VIEW[4], VIEW[5])
+
+    # 设置模型视图
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+    # 几何变换
+    glScale(SCALE_K[0], SCALE_K[1], SCALE_K[2])
+
+    # 设置视点
+    gluLookAt(
+        EYE[0], EYE[1], EYE[2],
+        LOOK_AT[0], LOOK_AT[1], LOOK_AT[2],
+        EYE_UP[0], EYE_UP[1], EYE_UP[2]
+    )
+
+    # 设置视口
+    glViewport(0, 0, WIN_W, WIN_H)
--- test.py
+++ test.py
@@ -1,61 +1,11 @@
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
-import numpy as np
 import interact
 
-def init():
-    glClearColor(0.0, 0.0, 0.0, 1.0)  # 设置画布背景色。注意：这里必须是4个参数
-    glEnable(GL_DEPTH_TEST)  # 开启深度测试，实现遮挡关系
-    glDepthFunc(GL_LEQUAL)  # 设置深度测试函数（GL_LEQUAL只是选项之一）
-
-
 def draw():
-    global IS_PERSPECTIVE, VIEW
-    global EYE, LOOK_AT, EYE_UP
-    global SCALE_K
-    global WIN_W, WIN_H
+    interact.drawInit()
 
-    # 清除屏幕及深度缓存
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-
-    # 设置投影（透视投影）
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-
-    if WIN_W > WIN_H:
-        if interact.IS_PERSPECTIVE:
-            glFrustum(interact.VIEW[0] * WIN_W / WIN_H, interact.VIEW[1] * WIN_W / WIN_H, interact.VIEW[2], interact.VIEW[3],
-                      interact.VIEW[4], interact.VIEW[5])
-        else:
-            glOrtho(interact.VIEW[0] * WIN_W / WIN_H, interact.VIEW[1] * WIN_W / WIN_H, interact.VIEW[2], interact.VIEW[3],
-                    interact.VIEW[4], interact.VIEW[5])
-    else:
-        if interact.IS_PERSPECTIVE:
-            glFrustum(interact.VIEW[0], interact.VIEW[1], interact.VIEW[2] * WIN_H / WIN_W, interact.VIEW[3] * WIN_H / WIN_W,
-                      interact.VIEW[4], interact.VIEW[5])
-        else:
-            glOrtho(interact.VIEW[0], interact.VIEW[1], interact.VIEW[2] * WIN_H / WIN_W, interact.VIEW[3] * WIN_H / WIN_W,
-                    interact.VIEW[4], interact.VIEW[5])
-
-    # 设置模型视图
-    glMatrixMode(GL_MODELVIEW)
-    glLoadIdentity()
-
-    # 几何变换
-    glScale(interact.SCALE_K[0], interact.SCALE_K[1], interact.SCALE_K[2])
-
-    # 设置视点
-    gluLookAt(
-        interact.EYE[0], interact.EYE[1], interact.EYE[2],
-        interact.LOOK_AT[0], interact.LOOK_AT[1], interact.LOOK_AT[2],
-        interact.EYE_UP[0], interact.EYE_UP[1], interact.EYE_UP[2]
-    )
-
-    # 设置视口
-    glViewport(0, 0, WIN_W, WIN_H)
-
-    # ---------------------------------------------------------------
     glBegin(GL_LINES)  # 开始绘制线段（世界坐标系）
 
     # 以红色绘制x轴
@@ -103,12 +53,6 @@ def draw():
     glutSwapBuffers()  # 切换缓冲区，以显示绘制内容
 
 
-def reshape(width, height):
-    global WIN_W, WIN_H
-
-    WIN_W, WIN_H = width, height
-    glutPostRedisplay()
-
 if __name__ == "__main__":
     glutInit()
     displayMode = GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH
@@ -118,9 +62,9 @@ if __name__ == "__main__":
     glutInitWindowPosition(300, 200)
     glutCreateWindow('Quidam Of OpenGL')
 
-    init()  # 初始化画布
+    interact.init()  # 初始化画布
     glutDisplayFunc(draw)  # 注册回调函数draw()
-    glutReshapeFunc(reshape)  # 注册响应窗口改变的函数reshape()
+    glutReshapeFunc(interact.reshape)  # 注册响应窗口改变的函数reshape()
     glutMouseFunc(interact.mouseclick)  # 注册响应鼠标点击的函数mouseclick()
     glutMotionFunc(interact.mousemotion)  # 注册响应鼠标拖拽的函数mousemotion()
     glutKeyboardFunc(interact.keydown)  # 注册键盘输入的函数keydown()
